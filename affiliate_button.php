d9d8e6f85189179607146df6aa25f03f956ed783
Sanjay Kumar <ksanjay69192@gmial.com>
2025-04-05 12:43:17 +0530

    Converted to static index.html with affiliate buttons

--- affiliate_button.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Check if affiliate_link exists and is not empty
-if (!empty($row['affiliate_link'])) {
-    echo "<div style='margin-top: 5px; font-size: 14px;'>Shop Now:</div>";
-    echo "<a href='" . htmlspecialchars($row['affiliate_link']) . "' target='_blank' class='button'>Buy on Amazon</a>";
-}
-?>
